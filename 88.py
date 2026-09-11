57d1865cd7a7983b9d40e0bfea446b3a421f3252
yayaya <D1515418@o365.fcu.edu.tw>
2026-09-11 11:52:53 +0800

    999

--- 88.py
+++ 88.py
@@ -1,1 +1,7 @@
 print("hello")
+print("黃文佑")
+print("D1515418")
+print("AITA")
+
+print("==" * 100)
+  
---
+++ 89.py/00.py
@@ -0,0 +1,8 @@
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+c = input("Enter a third number: ")
+
+print(int(a) * 5)
+print(int(a) * 2)
+print(int(b) * 4)
+print(int(c) * 6)
