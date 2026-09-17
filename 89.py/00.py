e65f049ba84c607a08855065a23e6fa71e51c268
yayaya <D1515418@o365.fcu.edu.tw>
2026-09-17 10:50:54 +0800

    D1515418.py

--- 89.py/00.py
+++ 89.py/00.py
@@ -6,3 +6,4 @@ print(int(a) * 5)
 print(int(a) * 2)
 print(int(b) * 4)
 print(int(c) * 6)
+
---
+++ D1515418.py
@@ -0,0 +1,12 @@
+for i in range(1, 5):
+    print("*" * i)
+for i in range(1):
+    print("-" * 10)
+width,weight = 5,5
+for _ in range(width):
+    print("*" * weight)
+for i in range(1):
+    print("-" * 10)
+spaces = " " * (4 - i)
+for i in range(4, -1,-1):
+    print("*" * i)
